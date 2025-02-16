49660c32b68389b91c76007d245de228df95a3bd
Adi_Tech <adityasinghxa07@gmail.com>
2025-02-17 00:10:58 +0530

    Add files via upload

---
+++ Que26.java
@@ -0,0 +1,25 @@
+class Animal {
+    Animal() {
+        System.out.println("Animal constructor called");
+    }
+}
+ 
+class Mammal extends Animal {
+    Mammal() {
+        super();
+        System.out.println("Mammal constructor called");
+    }
+} 
+class Dog extends Mammal {
+    Dog() {
+         super();
+        System.out.println("Dog constructor called");
+    }
+}
+ 
+public class Que26{
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+    }
+}
+ 
